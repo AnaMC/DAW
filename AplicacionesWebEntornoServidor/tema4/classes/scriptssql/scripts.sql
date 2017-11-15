d348317e6944dc73bc89b50f2324812a94e95f4e
Ana MC <troglodita91@gmail.com>
2017-11-15 11:33:41 +0000

    Subida Flex

--- AplicacionesWebEntornoServidor/tema4/classes/scriptssql/scripts.sql
+++ AplicacionesWebEntornoServidor/tema4/classes/scriptssql/scripts.sql
@@ -37,3 +37,10 @@ create table if not exists telefono (
     descripcion varchar(20) null,
     foreign key (idcontacto) references contacto(id) on delete restrict
 ) engine = innodb default character set = utf8 collate utf8_unicode_ci;
+
+create table if not exists usuario(
+    id bigint not null auto_increment primary key,
+    correo varchar (80) not null unique,
+    clave varchar (250) not null,
+    verificado tinyint(1) not null default (0),
+)engine = innodb default character set = utf8 collate utf8_unicode_ci;
