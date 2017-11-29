506363f03f7c7fa7f4f44b343243d0840b4f6622
Ana MC <troglodita91@gmail.com>
2017-11-29 13:37:02 +0000

    Actualización

--- AplicacionesWebEntornoCliente/JQuery/formularioJqueryHTML/script.js
+++ AplicacionesWebEntornoCliente/JQuery/formularioJqueryHTML/script.js
@@ -2,10 +2,15 @@ $(function(){
    
     $('form').submit(function(e){
         var c= validarCorreo();
-        var n= compararNombre();
+        // var n= compararNombre();
+        var p= validarPassword();
         
-        if(!c || !n){
+        /*Terminar de añadir*/
+            
+        if(!c || !p){ /*Terminar de añadir*/
             e.preventDefault();  //"Se para ....
+        }else{
+            alert ("enviado");
         }  
     });    
     
@@ -27,9 +32,18 @@ $(function(){
     function validarPassword(){
         
         var pass = ('input[name="password"]').val();
-        var estadoPass = false;
-  
         
+        var verifi = ('input[name="verificar"]').val();
+        var estadoVerifi = false;
+        
+        if (pass.length===6 && pass === verifi){
+            estadoVerifi = true;
+            
+        }else{
+            /*Crear nodo span y meterlo en el dom al lugar donde le corresponda*/ /*Modificar el DOM con jq*/
+            var span = $('.mensajePass').text('*Campo incorrecto, las contraseña deben tener 6 caracteres y ser iguales');
+        }
+        return estadoVerifi; 
     }
     
 });
