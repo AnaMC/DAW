532af372cc3ae62fa2306a6a5ad15d6b7ee34ca7
Ana MC <troglodita91@gmail.com>
2017-12-01 12:38:54 +0000

    Subida Ejercicio

--- AplicacionesWebEntornoCliente/JQuery/cambioHover/script.js
+++ AplicacionesWebEntornoCliente/JQuery/cambioHover/script.js
@@ -1,4 +1,28 @@
-$(document).ready(function () {
+$(document).ready(function() {
+    $("#foto1").hover(
+        function(){ // Función anonima de lo que va a pasar cuando el ratón entra en la foto, si quisieramos que tenga nombre se declara fuera "Como de toda la vida"
+            $(this).attr("src","h2.jpg"); /*.attr() Permite modificar un atributo del selector que deseemos*/
+        },
+        function(){
+             $(this).attr("src","h1.jpg");
+        } //// Función anonima de lo que va a pasar cuando el ratón sale de la foto
+        );
     
-
+      $("#foto2").hover(
+        function(){ 
+            $(this).attr("src","h6.jpg");
+        },
+        function(){
+             $(this).attr("src","h3.jpg");
+        } 
+        );
+    
+      $("#foto3").hover(
+        function(){
+            $(this).attr("src","h4.jpg");
+        },
+        function(){
+             $(this).attr("src","h5.jpg");
+        } 
+        );
 });
